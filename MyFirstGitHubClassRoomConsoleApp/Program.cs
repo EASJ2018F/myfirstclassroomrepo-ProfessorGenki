96d9cbdaa2a52111b1bbf3fb56548f470b0fed41
ProfessorGenki <getyowalkon@gmail.com>
2018-01-29 10:42:32 +0100

    Tilføjet nye linjer i Program.cs

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -12,6 +12,10 @@ namespace MyFirstGitHubClassRoomConsoleApp
         {
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+            Console.WriteLine("Yo Yo Martin! What Up Homie, Whatz Good Playa");
+
+            //Der stod ikke i opgaven at vi skulle have ReadKey delen med, men jeg satte den ind alligevel
+            Console.ReadKey();
         }
     }
 }
